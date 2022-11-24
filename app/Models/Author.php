83e2cc015dd402b30118c4a37fceb5dd2032836b
unfor <unforu0@gmail.com>
2022-11-24 12:19:39 +0300

    add references in Models

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Author extends Model
 {
     use HasFactory;
+
+    public function book()
+    {
+        return $this->hasMany(Book::class);
+    }
 }
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
 }
