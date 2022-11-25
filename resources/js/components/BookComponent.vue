5c23f2ad3fbd1fb803c6b89f692e11774cf6a751
unfor <unforu0@gmail.com>
2022-11-25 12:29:50 +0300

    create book component

--- resources/js/components/BookComponent.vue
+++ resources/js/components/BookComponent.vue
@@ -1,10 +1,27 @@
 <template>
-<div>Книга</div>
+<div class="book">
+    <img src="img/md.png" width="170" alt="Обложка" >
+    <h5>{{title}}</h5>
+        <div className="d-flex">
+            <div className="d-flex flex-column">
+                <span>Автор: {{author}}</span>
+                <b>Жанр: {{genre}}</b>
+            </div>
+        </div>
+</div>
 </template>
 
 <script>
 export default {
     name: "BookComponent",
+
+    data() {
+        return {
+            title: 'Отцы и дети',
+            author: 'Тургенев',
+            genre:  'Рассказ'
+        }
+    }
 }
 </script>
 
--- resources/js/components/HomeComponent.vue
+++ resources/js/components/HomeComponent.vue
@@ -1,6 +1,30 @@
 <template>
-<div>Все книги</div>
-<BookComponent/>
+<div class="content mx-3">
+<div class="first d-flex justify-content-between">
+    <h1 class="content__title my-3">Все книги</h1>
+    <div class="content__search">
+        <div class="form-floating" height="10px">
+            <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+                <option selected>По названию</option>
+                <option value="1">По автору</option>
+            </select>
+        <label for="floatingSelect">Параметры поиска</label>
+        </div>
+        <div className="search-block d-flex my-1">
+            <img src="/img/search.svg" alt="search" />
+            <img  className="clear cu-p" src="/img/btn-remove.svg" alt="Remove" >
+            <input  type="text" placeholder="Поиск..." >
+        </div>
+    </div>
+</div>
+<div class="d-flex flex-wrap">
+    <BookComponent/>
+    <BookComponent/>
+    <BookComponent/>
+    <BookComponent/>
+    <BookComponent/>
+</div>
+</div>
 </template>
 
 <script>
